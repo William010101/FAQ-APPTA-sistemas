ff7b86a06d6ba0b2a538432ed024958faa3c4692
Diego Ricardo Scheidt <drs5760@gmail.com>
2024-03-05 19:10:02 -0300

    modified:   php_action/db_connect.php

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,7 +1,25 @@
 <?php
-// Conexão com banco de dados
 
-$conn = pg_connect("host=localhost port=5432 dbname=FAQ user=postgres password=masterkey");
+function conectarBancoDeDados() {
+    $conn = null;
+    try {
+        // Conexão com banco de dados
+        $conn = pg_connect("host=127.0.0.1 port=5432 dbname=FAQ user=postgres password=masterkey");
 
+        if (!$conn) {
+            throw new Exception("Erro ao conectar ao banco de dados: " . pg_last_error());
+        }
 
+        return $conn;
+    } catch (Exception $e) {
+        // Tratamento de erro
+        echo "Ocorreu um erro: " . $e->getMessage();
+        exit;
+    }
+}
+
+// Chamada da função de conexão
+$conn = conectarBancoDeDados();
+
+// O código pode continuar a partir daqui usando a conexão $conn
 ?>
