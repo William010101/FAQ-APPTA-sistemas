59659ea6eea9a83cbd1abf911eacfbb3b9414736
Diego Ricardo Scheidt <drs5760@gmail.com>
2024-03-05 14:42:18 -0300

    localhost > 127.0.0.1 / senha 123 > masterkey

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 // Conexão com banco de dados
 
-$conn = pg_connect("host=localhost port=5432 dbname=FAQ user=postgres password=123");
+$conn = pg_connect("host=127.0.0.1 port=5432 dbname=FAQ user=postgres password=masterkey");
 
 if (!$conn) {
     echo "Erro ao conectar ao banco de dados.\n";
