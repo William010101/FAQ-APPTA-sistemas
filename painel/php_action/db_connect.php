ea69302c516891a575ecfdd4b8e336d7c8eb7287
William <drs5760@gmail.com>
2024-03-08 14:20:29 -0300

    Commit teste de nome

--- painel/php_action/db_connect.php
+++ painel/php_action/db_connect.php
@@ -2,7 +2,7 @@
 // Conexão com banco de dados
 
 
-$conn = pg_connect("host=localhost port=5432 dbname=FAQ user=postgres password=123");
+$conn = pg_connect("host=localhost port=5432 dbname=FAQ user=postgres password=masterkey");
 
 
 ?>
--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -1,25 +1,5 @@
 <?php
 
-function conectarBancoDeDados() {
-    $conn = null;
-    try {
         // Conexão com banco de dados
         $conn = pg_connect("host=localhost port=5432 dbname=FAQ user=postgres password=masterkey");
 
-        if (!$conn) {
-            throw new Exception("Erro ao conectar ao banco de dados: " . pg_last_error());
-        }
-
-        return $conn;
-    } catch (Exception $e) {
-        // Tratamento de erro
-        echo "Ocorreu um erro: " . $e->getMessage();
-        exit;
-    }
-}
-
-// Chamada da função de conexão
-$conn = conectarBancoDeDados();
-
-// O código pode continuar a partir daqui usando a conexão $conn
-?>
