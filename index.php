8329eb13f4f7dc9b9f8c3cd91e852cd4b9996b8b
william <williamlourenco273@gmail.com>
2024-07-26 23:20:28 -0300

    responsividade do input de pesquisa

--- index.php
+++ index.php
@@ -16,18 +16,18 @@ include_once 'include/ref.php';
     <section class="home-header ">
         <div class="container col-12" id="menupesquisa">
             <div class="botao">
-                <div class="row mt-1 ">
+                <div class="row  ">
                     <img class="col-6 mt-4" src="img\appta.png" alt="" style=" width: 90px; height: 100px;">
                     <h3 class="appta-titulo text-uppercase mt-4 mb-0 col-6"><span class="Poppins">Como podemos</span> <br><span class="PoppinsBold">ajudar?</span></h3>
-                    <div class=" col-12 mx-auto col-md-10 col-sm-12 col-lg-8 row">
+                    <div class=" col-12 mx-auto col-md-10 col-sm-12 col-lg-8 row mt-3">
 
-                        <div class="inputSearch col-12 ">
+                        <div class="inputSearch col-12">
                             <form method="POST" action="search">
-                                <div class="input-group mb-3 col-10">
-                                    <input type="search" class="form-control" name="pesquisar" placeholder="Tente buscar por tópicos ou palavras-chave" id="inputHeader">
-                                    <div class="input-group-append">
+                                <div class="input-group mb-3 col-12">
+                                    <input type="search" class="form-control w-75" name="pesquisar" placeholder="Tente buscar por tópicos ou palavras-chave" id="inputHeader">
+                                    
                                         <button class="btn" id="btnpesquisarhome" name="btnpesquisar" type="text"><i class="material-icons" id="">search</i></button>
-                                    </div>
+                                    
                                 </div>
                             </form>
                         </div>
